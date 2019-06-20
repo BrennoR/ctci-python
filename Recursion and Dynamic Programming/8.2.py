ee525c82cb1f3d359f853a6bf00f7656941ef73f
Brenno Ribeiro <brenno_ribeiro@my.uri.edu>
2019-06-19 23:39:21 -0400

    Added problem 8.2

--- Recursion and Dynamic Programming/8.2.py
+++ Recursion and Dynamic Programming/8.2.py
@@ -2,22 +2,24 @@
 
 
 # TODO: Fix algorithm
-def grid_recurse(grid, i, j, path):
-    path.append([i, j])
-    if grid[i][j] == 2:
+# Works but not exactly best path nor efficient
+def grid_recurse(grid, i, j, path, memo):
+    if grid[i][j] != 1:
+        path.append([i, j])
+        memo.add((i, j))
+        if 0 <= j < (len(grid[0]) - 1) and (i, j + 1) not in memo:
+            right = grid_recurse(grid, i, j + 1, path, memo)
+            if right and grid[right[-1][0]][right[-1][1]] == 2:
+                return right
+        if 0 <= i < (len(grid) - 1) and (i + 1, j) not in memo:
+            down = grid_recurse(grid, i + 1, j, path, memo)
+            if down and grid[down[-1][0]][down[-1][1]] == 2:
+                return down
         return path
-    if 0 <= i < (len(grid) - 1) and grid[i + 1][j] != 1:
-        down_path = grid_recurse(grid, i + 1, j, path)
-        if down_path:
-            return down_path
-    if 0 <= j < (len(grid[0]) - 1) and grid[i][j + 1] != 1:
-        right_path = grid_recurse(grid, i, j + 1, path)
-        if right_path:
-            return right_path
 
 
 def robot_in_grid(grid):
-    return grid_recurse(grid, 0, 0, [])
+    return grid_recurse(grid, 0, 0, [], set())
 
 
 grid_1 = [[0, 0, 0, 0, 0, 0],
@@ -29,8 +31,8 @@ grid_1 = [[0, 0, 0, 0, 0, 0],
 
 grid_2 = [[0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 1],
+          [0, 0, 0, 0, 1, 0],
           [0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 2]]
 
---
+++ Recursion and Dynamic Programming/8.3.py
@@ -0,0 +1,85 @@
+# Magic Index
+
+
+def magic_index(arr):
+    if not arr:
+        return False
+
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == mid:
+            return mid
+        elif arr[mid] > mid:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return False
+
+
+# FOLLOW-UP, non-distinct integers
+def magic_index_2(arr):
+    if not arr:
+        return False
+
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == mid:
+            return mid
+        else:
+            left = mid - 1
+            while left > start and arr[left] == arr[mid]:
+                left -= 1
+            if arr[left] == left:
+                return left
+
+            right = mid + 1
+            while right < end and arr[right] == arr[mid]:
+                right += 1
+            if arr[right] == right:
+                return right
+
+            if arr[right] > right and arr[left] > left:
+                start = right
+            else:
+                end = left
+
+    return False
+
+
+if __name__ == '__main__':
+    # m_index = 5
+    arr_1 = [-1, 0, 1, 2, 3, 5]
+
+    # no m_index
+    arr_2 = [-8, -7, -6, -5, -4, -3, -2]
+
+    # m_index = 4
+    arr_3 = [-3, -2, -1, 0, 4, 9, 10, 15]
+
+    # empty array
+    arr_4 = []
+
+    # null
+    arr_5 = None
+
+    print(magic_index(arr_1))
+    print(magic_index(arr_2))
+    print(magic_index(arr_3))
+    print(magic_index(arr_4))
+    print(magic_index(arr_5))
+
+    # Non-distinct values
+    # m_index = 5
+    arr_6 = [1, 2, 4, 4, 5, 5]
+    print(magic_index_2(arr_6))
+
+    arr_7 = [2, 2, 3, 2, 2, 5]
+    print(magic_index_2(arr_7))
+
+    arr_8 = [1, 8, 8, 8, 8, 8, 9, 9, 9, 10, 10, 11]
+    print(magic_index_2(arr_8))
